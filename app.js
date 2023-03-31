65f074e749efaf7b181228c5e25d3632f4a4ea2a
santeripesonen <santerip55@gmail.com>
2023-03-31 20:22:53 +0300

    Ajax calls and styling changes

--- app.js
+++ app.js
@@ -12,64 +12,66 @@ app.use(express.static('Assets'));
 // for parsing application/xwww-
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-// Define the home page route
-
-
 app.get('/messages', (req, res) => {
+  // returns messages in json form to guestbook
   const messages = JSON.parse(fs.readFileSync("./messagedata.json", {encoding: "utf8"}))
   res.json(messages)
 })
 
 app.get('/', (req, res) => {
+  // returns frontpage file
     res.sendFile(path.join(__dirname, '/Assets/index.html'));
 });
 
-// Define the guestbook page route
 app.get('/guestbook', (req, res) => {
+  // returns guestbook file
   res.sendFile(path.join(__dirname, '/Assets/guestbook.html'));
 });
 
-// Define the new message page route
 app.get('/newmessage', (req, res) => {
+  // returns new message form
   res.sendFile(path.join(__dirname, '/Assets/newmessage.html'))
 });
 
 app.post('/newmessage', (req, res) => {
+  // receives a post request and creates a new message
+  // reads the data from the request
   const {username, country, message} = req.body
   const messages = JSON.parse(fs.readFileSync("./messagedata.json", {encoding: "utf8"}))
   const date = Date.now()
+  // add new message to list
   messages.push({id: messages[messages.length-1].id + 1, username, country, message, date })
 
+  //writes the updated messagelist to json file
   fs.writeFileSync("./messagedata.json", JSON.stringify(messages), null, 4)
 
+  // returns success page
   res.sendFile(path.join(__dirname, '/Assets/sentmessage.html'))
 });
 
 app.get('/ajaxmessage', (req, res) => {
+  // returns ajaxmessage page
   res.sendFile(path.join(__dirname, '/Assets/ajaxmessage.html'))
 });
 
 app.post('/ajaxmessage', (req, res) => {
+  // receives a post request and creates a new message
+  // reads the data from the request
   const {username, country, message} = req.body
-  console.log("kana", req.body)
   const messages = JSON.parse(fs.readFileSync("./messagedata.json", {encoding: "utf8"}))
   const date = Date.now()
+
+   // add new message to list
   messages.push({id: messages[messages.length-1].id + 1, username, country, message, date })
 
+  //writes the updated messagelist to json file
   fs.writeFileSync("./messagedata.json", JSON.stringify(messages), null, 4)
 
-  res.json({message: "Message received"})
+  // returns json with the updated list
+  res.json({messages: messages})
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
-
-/*
-Tee sivut: index.html, guestbook.html ja ajaxmessage.html
-index.html sisältö: sisältää navigaatiolinkit muihin sivuihin
-guestbook.html: näyttää olemassa olevat viestit
-ajaxmessage.html: sama kuin newmessage mutta lähetyslogiikka eri
-
-*/
